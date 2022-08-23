8ab74cc56fc0e254445932164136b78b72e52e76
RoMcGill <ro.mcgill12@gmail.com>
2022-08-23 22:21:46 +0000

    finish styling navbar/make site more responsive

--- static/js/app.js
+++ static/js/app.js
@@ -3,3 +3,9 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
 });
+
+const mobilemenu = document.querySelector('.mobilemenu');
+
+hamburger.addEventListener('click', function () {
+    mobilemenu.classList.toggle('is-open');
+});
