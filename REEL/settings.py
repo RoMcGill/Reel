fa35c283b5f7590915e5b1d97d882ee29fd06b0e
RoMcGill <ro.mcgill12@gmail.com>
2022-08-24 20:51:48 +0000

    fix syntax error in settings.py

--- REEL/settings.py
+++ REEL/settings.py
@@ -177,7 +177,7 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticno')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
