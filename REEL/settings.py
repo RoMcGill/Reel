7774676ad6c0d92fd4b8f54b6d7762e1a235bbed
RoMcGill <ro.mcgill12@gmail.com>
2022-08-27 17:21:50 +0000

    removed allow all users from settings.py

--- REEL/settings.py
+++ REEL/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["reel-2022-new.herokuapp.com", "localhost", "*"]
+ALLOWED_HOSTS = ["reel-2022-new.herokuapp.com", "localhost"]
 CSRF_TRUSTED_ORIGINS = [
     'https://*.gitpod.io']
 
